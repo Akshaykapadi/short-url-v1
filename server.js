6e2c31e6e5dcf4ab4c5c2003e3f0ce14fd8fa9ed
Akshay Kapadi <akshaykapadi8@gmail.com>
2019-11-29 21:08:04 +0530

    testing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 // init
 const app = express();
 
-app.use(express.static(path.join(__dirname,"client/build"))) 
+const port = process.env.PORT || 5000;
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -45,10 +45,17 @@ app.get('/:hash',(req,res) => {
 })
 
 // path
-app.get('*',(req, res)=>{
-    res.sendFile(path.join(__dirname + "/client/build/index.html")) 
-});
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
 
 // Port
-const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on ${port}`));
